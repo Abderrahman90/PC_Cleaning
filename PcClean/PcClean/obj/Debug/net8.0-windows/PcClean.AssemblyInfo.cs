ef623faa4d1a2b64bc4795c075a17dd80297ab2e
Abderrahman90 <abdethe90@gmail.com>
2025-06-13 16:51:05 +0200

    commit des fichiers essentiels

--- PcClean/PcClean/obj/Debug/net8.0-windows/PcClean.AssemblyInfo.cs
+++ PcClean/PcClean/obj/Debug/net8.0-windows/PcClean.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PcClean")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0f718e6be812e5d8c3749bf8e69057cab212389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d054868d5ce7f3556b13fbf6208bd29d3fdd73e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PcClean")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PcClean")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
